d01ecb69ae3cde917e10ce54d39cc45d2bb0e053
abearducci <bearprod2022@gmail.com>
2023-02-06 22:11:20 -0500

    Initial Commit

---
+++ v1_ui/v1_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  v1_uiApp.swift
+//  v1_ui
+//
+//  Created by Alex Bearducci on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct v1_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
